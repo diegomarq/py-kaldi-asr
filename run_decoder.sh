46eec61c424aef878cbd1e90f3617b656f7dc6c3
Diego Marques <diegomarques@linearc.com.br>
2021-12-18 12:09:08 -0300

    ROOT as opt/py-kaldi-asr

--- run_decoder.sh
+++ run_decoder.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ROOT=/opt/py-kaldi-asr
+
 export LD_PRELOAD=/opt/intel/mkl/lib/intel64/libmkl_def.so:/opt/intel/mkl/lib/intel64/libmkl_avx2.so:/opt/intel/mkl/lib/intel64/libmkl_core.so:/opt/intel/mkl/lib/intel64/libmkl_intel_lp64.so:/opt/intel/mkl/lib/intel64/libmkl_intel_thread.so:/opt/intel/lib/intel64_lin/libiomp5.so
 
-python3 run_decoder.py
+python3 $ROOT/run_decoder.py
