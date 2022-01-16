306261671231ba7bb9820190ab2e6ad2991590de
Diego Marques <diegomarques@linearc.com.br>
2022-01-16 11:01:50 -0300

    run_decoder.py/run_decoder.sh deleted

--- run_decoder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ROOT=/opt/py-kaldi-asr
-
-export LD_PRELOAD=/opt/intel/mkl/lib/intel64/libmkl_def.so:/opt/intel/mkl/lib/intel64/libmkl_avx2.so:/opt/intel/mkl/lib/intel64/libmkl_core.so:/opt/intel/mkl/lib/intel64/libmkl_intel_lp64.so:/opt/intel/mkl/lib/intel64/libmkl_intel_thread.so:/opt/intel/lib/intel64_lin/libiomp5.so
-
-python3 $ROOT/run_decoder.py
